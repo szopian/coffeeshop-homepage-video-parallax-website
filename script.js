f4dafeb90d89ade4d44c63bcef0eb6d95fcf15d5
Adri Ana <szopiangreg@gmail.com>
2021-03-16 10:42:35 +0000

    add listern onscrollView method

--- script.js
+++ script.js
@@ -13,8 +13,30 @@ function myFunction() {
   }
 }
 
-// const leftImg = document.querySelector("#left");
-// leftImg.addEventListener("click", () => {
+const leftImg = document.querySelector("#left");
+leftImg.addEventListener("click", () => {
+  console.log("clicked video-1");
+  const video1 = document.getElementById("video-1");
+  video1.scrollIntoView();
+});
 
-//   console.log("clicked left");
-// });
+const centerLeftImg = document.querySelector("#center-left");
+centerLeftImg.addEventListener("click", () => {
+  console.log("clicked center-left");
+  const img2 = document.getElementById("img2");
+  img2.scrollIntoView();
+});
+
+const centerRightImg = document.querySelector("#center-right");
+centerRightImg.addEventListener("click", () => {
+  console.log("clicked center-right");
+  const img3 = document.getElementById("img3");
+  img3.scrollIntoView();
+});
+
+const rightImg = document.querySelector("#right");
+rightImg.addEventListener("click", () => {
+  console.log("clicked right");
+  const img4 = document.getElementById("img4");
+  img4.scrollIntoView();
+});
